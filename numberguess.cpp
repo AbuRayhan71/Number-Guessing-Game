aeda12621abc5d12aff66aae62bab587db6f2c11
abura <mdaburayhanict@gmail.com>
2024-01-24 14:58:43 +1100

    just the most basic working version

---
+++ numberguess.cpp
@@ -0,0 +1,22 @@
+using namespace std;
+#include <iostream>
+int main()
+{
+    int A=899;
+    int i,n;
+    cout<<"enter number";
+    cin >> n ;
+   
+     if (i > A){
+         
+     cout << " input is bigger  ";}
+     if(i = A){
+         
+     cout << " you got it mate , amazing work!!!";}
+     else
+     {cout<<"our number is bigger";}
+  
+
+    return 0;
+}
+// I just used the if else logic that i understood 
